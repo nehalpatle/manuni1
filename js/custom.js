8c41e78b8595b0dfcf53029b2ad6b97ffd77b951
DNS ADS LAGAO <96223051+nehalpatle@users.noreply.github.com>
2022-04-10 15:07:27 +0530

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -363,4 +363,20 @@ $(document).ready(function() {
         var value = $('#confirmnumber').val();
         return $.isNumeric(value);
     });
-});
+}); 
+//   collapse faq
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
